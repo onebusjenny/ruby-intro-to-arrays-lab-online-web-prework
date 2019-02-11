654409456752444a2f2f0b6d8d9a950c953bdfec
Jenny Chu <jchudesign0930@gmail.com>
2019-02-11 07:21:43 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array 
+ instantiate_new_array = []
+ 
+end
+
+def array_with_two_elements 
+  array = ["hello","goodbye"]
+end
+
+def first_element (array)
+  array = ["hello","Welcome to New York"]
+  array.pop
+end
+
+def third_element (array)
+  array = ["hello","Welcome to New York","Style"]
+  array [2]
+end
+
+def last_element (array)
+  array = ["hello","Out of The Woods","Style"]
+  array [-2]
+end
+
+def first_element_with_array_methods (array)
+ array = ["hello","Thailand"]
+ array.pop
+end
+
+def last_element_with_array_methods (array)
+  array = ["Myanmar","goodbye"]
+  array.shift
+end
+
+def length_of_array(array)
+  array = ["hello","goodbye","see","apple","Yes","No","Hi","Wood"]
+  array.count
+end
+
+  
